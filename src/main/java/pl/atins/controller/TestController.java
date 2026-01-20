b87f818c75bf57866744ea618d720525839c2270
przemyslaw-orpel <przemo.orpel@gmail.com>
2026-01-20 22:41:34 +0100

    web for tomcat9 demo

---
+++ src/main/java/pl/atins/controller/TestController.java
@@ -0,0 +1,15 @@
+package pl.atins.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+
+    @GetMapping("/test")
+    @ResponseBody
+    public String test() {
+        return "Spring MVC dziala";
+    }
+}
